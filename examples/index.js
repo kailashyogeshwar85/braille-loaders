660b8f3d54a3ce86e60b5bddef7eb7581cfd86bb
kailashyogeshwar85 <kaialshyogeshwar85@gmail.com>
2017-05-09 16:03:00 +0530

    Stable release version 1.0.0 with full 64 loaders support

---
+++ examples/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var BrailleLoader = require('../'); 
+var spinner  = null;
+var timeout  = 2000;
+var index    =  0;
+var colors   = ['red','green','blue','purple','yellow','cyan','white']; 
+var spinners = Object.keys(BrailleLoader.spinners);
+var total    = spinners.length;
+
+function execute(_timeout){
+	_timeout = _timeout || timeout;
+	
+	setTimeout(function (){
+		if(spinner !== null){
+			spinner.stop();
+		}
+		spinner = new BrailleLoader({
+			spinner: spinners[index],
+			color: colors[ Math.floor(Math.random() * colors.length)],
+			text: 'Loader ' + (index + 1) + ' of ' + total 
+		});
+		spinner.start();
+
+		if(index < total){
+			index++;
+			execute();
+		} else {
+			process.stdout.write("\n\n all spinners done");
+			spinner.stop();
+		}
+	}, _timeout); 
+}
+
+execute(0);
---
+++ index.js
@@ -0,0 +1,112 @@
+'use strict';
+
+const readline         = require('readline');
+const _                = require('lodash');
+const colors           = require('./colors');
+const utils            = require('util'); 
+const spinners         = require('cli-spinners')
+BrailleLoader.spinners = spinners;
+
+var _defaultOptions = {
+	color: 'white',
+	frames: null,
+	currentFrame: 0,
+	speed: 80,
+	spinner: 'dots',
+	spinnerId: 0,
+	text: ''
+};
+
+
+function BrailleLoader(options){
+	if (typeof options === 'string'){
+		this.options = _.extend({}, _defaultOptions, { 'text': options });
+		this.options.frames = this.getSpinner(_defaultOptions.spinner).frames;
+		this.options.speed = this.getSpinner(_defaultOptions.spinner).interval;
+	} else {
+		this.options = _.extend({}, _defaultOptions, options);
+		this.options.frames = this.getSpinner(this.options.spinner).frames;
+		this.options.speed = this.getSpinner(this.options.spinner).interval;
+	}
+}
+
+BrailleLoader.prototype.start = function(){
+	var self = this;
+	process.stdout.write(self.options.frames[self.options.currentFrame]);
+  process.stdout.write('\x1b[?25l');// blink cursor off	
+	self.options.spinnerId = setInterval(function(){	
+		self.paintFrames()
+	},self.options.speed);
+}
+
+BrailleLoader.prototype.stop = function(){
+	this.destroy();
+}
+
+BrailleLoader.prototype.destroy = function(){
+	clearInterval(this.options.spinnerId);
+	resetPrompt();
+}
+
+BrailleLoader.prototype.getColor = function(){
+	return colors[this.options.color.toLowerCase()] || '\x1b[37m';
+}
+
+BrailleLoader.prototype.setColor = function(color){
+	this.color = color;
+}
+
+BrailleLoader.prototype.paintFrames = function(){
+	this.clear(); // clear previous output
+	var colorCode = this.getColor();
+	this.options.currentFrame++;
+	if(this.options.currentFrame >= this.options.frames.length){
+		this.options.currentFrame	 = 0;
+	}
+	readline.moveCursor(process.stdout, -this.options.frames[this.options.currentFrame].length , 0);
+	var str = `${this.getColor()}${this.options.frames[this.options.currentFrame]} ${this.options.text}`
+
+	process.stdout.write('\r' + str);
+}
+
+BrailleLoader.prototype.getSpinner = function(spinner){
+	if (!spinners[spinner]){
+		console.warn('Invalid spinner type refer cli-spinners for all types');
+		process.exit(0);
+	}
+	return spinners[spinner];
+}
+
+BrailleLoader.prototype.clear = function(){
+	process.stdout.clearLine();
+}
+
+BrailleLoader.prototype.setText = function(str){
+	this.options.text = str;
+}
+
+function resetPrompt(){
+	process.stdout.write('\x1B[?25h')//show's the cursor
+	process.stdout.write('\x1B[0m') 
+	process.exit(0);	
+}
+
+process.on('SIGINT', resetPrompt);
+
+module.exports = BrailleLoader;
+
+var l = new BrailleLoader({
+	spinner: 'moon',
+	color: 'green',
+	text: 'Loading'
+})
+
+l.start();
+
+setTimeout(function(){
+	l.setText("watching moons");
+	setTimeout(function(){
+		console.log('stopping')
+		l.stop()
+	},5000)
+},5000)
